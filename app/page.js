2c032f7fe2d59d3c0bada0b7c46b702dfde90d81
39jorgegonzalez-lab <39jorgegonzalez@gmail.com>
2026-05-01 19:04:16 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>MyLoanPreview</h1>
+      <p>Your loan preview platform is coming soon.</p>
+    </main>
+  );
+}
